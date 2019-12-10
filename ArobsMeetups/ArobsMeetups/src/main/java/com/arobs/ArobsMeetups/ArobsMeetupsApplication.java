cde65c77c588f7f9e629251c458d7ca1fc3ad17c
Robert Vinter <robert.vinter@arobs.com>
2019-12-10 13:30:23 +0200

    First Commit Initial App

---
+++ ArobsMeetups/ArobsMeetups/src/main/java/com/arobs/ArobsMeetups/ArobsMeetupsApplication.java
@@ -0,0 +1,13 @@
+package com.arobs.ArobsMeetups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArobsMeetupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArobsMeetupsApplication.class, args);
+	}
+
+}
---
+++ ArobsMeetups/ArobsMeetups/src/test/java/com/arobs/ArobsMeetups/ArobsMeetupsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arobs.ArobsMeetups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArobsMeetupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
