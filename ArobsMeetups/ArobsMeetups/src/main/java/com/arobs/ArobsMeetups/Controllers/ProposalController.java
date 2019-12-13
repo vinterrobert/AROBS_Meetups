7c3f09afa53492a0e7c6dfc9d753c80716311240
Robert Vinter <robert.vinter@arobs.com>
2019-12-13 16:28:05 +0200

    Third Commit. Some endpoints for User and Proposal

--- ArobsMeetups/ArobsMeetups/src/main/java/com/arobs/ArobsMeetups/Controllers/ProposalController.java
+++ ArobsMeetups/ArobsMeetups/src/main/java/com/arobs/ArobsMeetups/Controllers/ProposalController.java
@@ -1,15 +1,55 @@
 package com.arobs.ArobsMeetups.Controllers;
 
+import com.arobs.ArobsMeetups.Entities.Proposal;
 import com.arobs.ArobsMeetups.Services.ProposalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("/proposal")
+@RequestMapping("/proposals")
 public class ProposalController {
 
     @Autowired
     ProposalService proposalService;
+
+    @GetMapping(path="/", produces = "application/json")
+    public List<Proposal> getAllProposals(){
+
+        return null;
+    }
+
+    @PostMapping(path= "/", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> addProposal(@RequestBody Proposal newProposal) {
+
+        return null;
+    }
+
+    @DeleteMapping(path="/{idProposal}")
+    public ResponseEntity<Object> deleteProposal(@PathVariable int idProposal){
+
+        return null;
+    }
+
+    @GetMapping(path="/user/{idUser}/proposals", produces = "application/json")
+    public List<Proposal> getUserProposal(@PathVariable int idUser){
+
+        return null;
+    }
+
+    @GetMapping (path = "/{difficulty}/proposals", produces = "application/json")
+    public List<Proposal> getProposalsOfSameDifficulty(@PathVariable String difficulty){
+
+        return null;
+    }
+
+    @GetMapping (path = "/{type}/proposals", produces = "application/json")
+    public List<Proposal> getProposalsOfSameType(@PathVariable String type){
+
+        return null;
+    }
 
 }
--- ArobsMeetups/ArobsMeetups/src/main/java/com/arobs/ArobsMeetups/Controllers/UserController.java
+++ ArobsMeetups/ArobsMeetups/src/main/java/com/arobs/ArobsMeetups/Controllers/UserController.java
@@ -1,15 +1,50 @@
 package com.arobs.ArobsMeetups.Controllers;
 
+import com.arobs.ArobsMeetups.Entities.User;
 import com.arobs.ArobsMeetups.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/user")
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     UserService userService;
+
+    @GetMapping(path="/", produces = "application/json")
+    public List<User> getAllUsers(){
+
+        return null;
+    }
+
+    @PostMapping(path= "/", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> addUser(@RequestBody User newUser) {
+
+        return null;
+    }
+
+    @DeleteMapping(path="/{idUser}")
+    public ResponseEntity<Object> deleteUser(@PathVariable int idUser){
+
+        return null;
+    }
+
+    @GetMapping(path="/userId{idUser}", produces = "application/json")
+    public User getUserById(@PathVariable int idUser){
+
+        return null;
+    }
+
+    @GetMapping (path = "/userEmail{email}", produces = "application/json")
+    public User getUserByEmail(@PathVariable String email){
+
+        return null;
+    }
+
+
 
 }
